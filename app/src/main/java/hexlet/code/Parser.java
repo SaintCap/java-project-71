0752b35e9ba4264ea61a35f718c0b3bdc997ac7e
Stas Steblyanskiy <ssteblyanskiy@bk.ru>
2025-11-10 09:58:41 +0300

    Simplified the implementation of mapping (style fix)

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,9 +7,9 @@ public final class Parser {
 
     public static ObjectMapper getMapper(String extension) {
         return switch (extension) {
-                case "json" -> new ObjectMapper();
-                case "yaml", "yml" -> new YAMLMapper();
-                default -> throw new RuntimeException("Unsupported file extension: " + extension);
+            case "json" -> new ObjectMapper();
+            case "yaml", "yml" -> new YAMLMapper();
+            default -> throw new RuntimeException("Unsupported file extension: " + extension);
         };
     }
 
